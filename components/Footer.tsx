087df518654c36f1ad2db32b14dfd2e1575d666f
weilin <william@aiii.ai>
2025-05-02 11:34:11 +0800

    fix: update footer copyright year to 2025

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-	<p className="text-center text-xs text-gray-500 mb-2">© 2023 weilindev. All Rights Reserved.</p>
+	<p className="text-center text-xs text-gray-500 mb-2">© 2025 weilindev. All Rights Reserved.</p>
 )
 
 export default Footer
