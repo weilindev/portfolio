0281b2348c35bfe424144cc4bab135799859b824
weilin <william@aiii.ai>
2025-05-02 11:33:49 +0800

    fix: update PERSONAL_TAG constants to include new technologies
    - Replaced 'React.js' with 'MongoDB' as the first entry.
    - Added 'Firebase' and 'GCP' to the list of technologies.
    - Updated the icon options to include 'FaFire' and 'FaGoogle'.
    - Rearranged the order of entries for better organization.

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,7 @@
-export const PERSONAL_TAG: { id: string, title: string, icon?: 'FaHashtag' | 'FaReact' | 'FaDatabase' }[] = [
-	{ id: '01',title: 'React.js', icon: 'FaReact' },
-	{ id: '02',title: 'MongoDB', icon: 'FaDatabase' },
-	{ id: '03',title: 'Software Engineer' },
+export const PERSONAL_TAG: { id: string, title: string, icon?: 'FaHashtag' | 'FaReact' | 'FaDatabase' | 'FaFire' | 'FaGoogle' }[] = [
+	{ id: '01',title: 'MongoDB', icon: 'FaDatabase' },
+	{ id: '02',title: 'Firebase', icon: 'FaFire' },
+	{ id: '03',title: 'GCP', icon: 'FaGoogle' },
+	{ id: '04',title: 'React.js', icon: 'FaReact' },
+	{ id: '05',title: 'Software Engineer' },
 ]
